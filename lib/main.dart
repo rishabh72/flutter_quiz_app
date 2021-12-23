43762404f89725240bac53ec95b3b9f6ca67c2f3
rishabh <sonirishabh018@gmail.com>
2021-12-23 15:25:22 +0530

    class constructor and named arguments

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,48 @@
-// Flutter is all about widgets for example AppBar Widget, List Widget, ListItem Widget
-
 import 'package:flutter/material.dart';
 
-void main() {
-  // runApp (material.dart)
-  // runApp takes Widget Tree and draws on screen
-  runApp(MyApp());
-}
+// void main() {
 
-//Creating Root Widget
-// Flutter/material.dart has StatelessWidget class so we import material.dart file
+//   runApp(MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Text("Hi"),
-    ); // MaterialApp from 'material.dart. MaterialApp uses named arguments
-    // we have used Text widget buitin widget in flutter
-  }
-}
+// class MyApp extends StatelessWidget {
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Text("First Line In Flutter"),
+//     );
+//   }
+// }
+
+// Constructor
+
+// class Person {
+//   String name;
+//   String age;
+
+//   Person(String inputName, int age) {
+//     name = inputName;
+//     this.age = age;
+//   }
+// }
+
+// var p1 = new Person('max', 12);
+
+// class Person {
+//   String name;
+//   String age;
+
+//   //named arguments
+//   Person({@required String inputName, int age = 30}) {
+//     name = inputName;
+//     this.age = age;
+//   }
+// }
+
+// var p2 = new Person(inputName: 'max',age: 12);
+
+// class Person {
+//   String name;
+//   int age;
+
+//   Person({this.name, this.age = 23});
+// }
