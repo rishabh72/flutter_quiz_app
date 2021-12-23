0d8a3f590dde8b8f9a4715a1f26f40d30ecd427e
rishabh <sonirishabh018@gmail.com>
2021-12-23 14:18:30 +0530

    building app from scratch

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,21 @@
-//main is a function that runs first automatically by flutter
+// Flutter is all about widgets for example AppBar Widget, List Widget, ListItem Widget
 
-void main() {}
+import 'package:flutter/material.dart';
 
-// we use camelcase to give name for example addTwoNumbers
-// we use semicolon after expressin except after curely braces
-// we use print to console data
-// dart has dynamic type which is similar to any type of typescript
+void main() {
+  // runApp (material.dart)
+  // runApp takes Widget Tree and draws on screen
+  runApp(MyApp());
+}
 
-//dart types
-// text = strings, type = String
-// integer = 23 , type = int
-// float = 3.45, type = double
-// there is another type exists that is "num" which accepts both integers and float
-// void type = nothing return
+//Creating Root Widget
+// Flutter/material.dart has StatelessWidget class so we import material.dart file
 
-// functions in dart
-// void addNums(int a, int b) {
-//   print(a + b);
-// }
-
-// int getAddition(num a, num b) {
-//   return a + b;
-// }
-
-//variables in dart
-
-// var fruit = 1;
-
-// we can use int fruit = 1; but dart has a feature called type-inference so dart is able to infer data types by the value on right side
-// so declaring with var is better practice;
-
-// double mango; -> in this case declaring with double is better
-// mango = 3.4;
-
-// Dart is object oriented programming
-// creating person object from class
-// class Person {
-//   var name = 'max';
-//   var age = 2;
-// }
-
-// const p1 = new Person();
-// const p2 = Person(); // we can create objects without new keyword
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text("Hi"),
+    ); // MaterialApp from 'material.dart. MaterialApp uses named arguments
+    // we have used Text widget buitin widget in flutter
+  }
+}
