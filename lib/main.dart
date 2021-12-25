604095a018757e9c1ae3f72629405eb2bab0bcd1
rishabh <sonirishabh018@gmail.com>
2021-12-25 10:30:06 +0530

    updating correctly with stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 
-// Column widget is used to render widgets in column
+// Stateless Widget
+// input data ==> Widget ==> Renders UI(Gets rerendered when input data changes)
 
-// list are another type of data type in dart
-// var list = ['hello','man']
+// Stateful Widget
+// input data ==> Widget[internal state] ==> Renders UI(Gets rerendered when input data or local state changes)
+
+// We can convert stateless widget into stateful widget using shift + control + R
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
   void answerQuestion() {
-    questionIndex += 1;
+    setState(() {
+      questionIndex += 1;
+    });
     print(questionIndex);
   }
 
