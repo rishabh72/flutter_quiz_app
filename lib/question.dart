28043615a1e116b1c1b73e14b08fab3be65d80c7
rishabh <sonirishabh018@gmail.com>
2021-12-26 15:01:06 +0530

    enums, constructorsand styling of question text

--- lib/question.dart
+++ lib/question.dart
@@ -7,6 +7,41 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      //Container widget is used to give margin, padding, background color, border
+      width: double.infinity, // double.infinity = 100%
+      child: Text(
+        //Here Text widget has a width = its parent width
+        questionText,
+        style: TextStyle(fontSize: 18),
+        textAlign: TextAlign.center,
+      ),
+      // color: Colors.green,
+      margin: EdgeInsets.all(15),
+    );
   }
 }
+
+
+// Enums
+// enum Status{
+//   started,
+//   working,
+//   completed,
+//   finished
+// }
+
+// class Person{
+//   String name;
+//   int birthYear;
+//   String gender;
+  
+//   Person(this.name,this.birthYear,this.gender);
+  
+//   Person.twentyYearsPerson(this.name):birthYear = 1990, gender='male';
+  
+// }
+
+//   var p1 = Person.twentyYearsPerson("Rishabh");
+
+
