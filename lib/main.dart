d01c775be4fd4a0e21be3e58d52347caca65072c
rishabh <sonirishabh018@gmail.com>
2021-12-28 00:34:14 +0530

    if-else, &&, ||, ternary and null safety

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,26 @@ import 'package:flutter/material.dart';
 import 'question.dart';
 import 'answer.dart';
 
-//final vs const
-// final is run time constant but const is both compile time and run time constant.
-// Check out Question widget,at the point of
-// writing value of questionText is not known,
-// it is like dynamic but when code run(compiles) value is locked into widget
-// var list = const [11,12,13]; ==> list = [1,2,3] works but list.add(12) gives error
-// const list = [11,12,13]; => if we change dart gives error
+// if-else condition,if-else if-else if condition, && operator, || operator exists in dart
+
+// null value
+// var username; // username is null
+// username = "max";
+// Or we can explicitly declare username = null
+
+// Null safety => dart(2.12.0)
+// variables can not contain null unless you say they can.
+
+// Null assertion operator
+// If you are sure that expression with nullable type is not null
+// you can use ! operator.
+
+// String name = "rishabh";
+// name = null --> gives error
+// To tell dart that "name" can hold string or null value
+// String? name = "rishabh";
+
+// Ternary operator syntax condion1 ? action1 : action2
 
 void main() => runApp(MyApp());
 
@@ -20,55 +33,59 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  var questions = const [
+    {
+      'questionText': 'Which is a synonym of synthesis?',
+      'answers': ['picture', 'imitation', 'split', 'blend'],
+      'correctAnswer': 'blend'
+    },
+    {
+      'questionText': "Which is a synonym of epidemic?",
+      'answers': ['artificial', 'swift', 'infectious', 'mocking'],
+      'correctAnswer': 'infectious'
+    },
+    {
+      'questionText': 'Which is a synonym of refrain?',
+      'answers': ['continue', 'abstain', 'subtract', 'permit'],
+      'correctAnswer': 'abstain'
+    },
+    {
+      'questionText': 'Which is a synonym of conjecture?',
+      'answers': ['approval', 'demonstration', 'weight', 'guess'],
+      'correctAnswer': 'guess'
+    }
+  ];
+
   void _answerQuestion() {
     setState(() {
       _questionIndex += 1;
     });
-    print(_questionIndex);
   }
 
   var _questionIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    var questions = const [
-      {
-        'questionText': 'Which is a synonym of synthesis?',
-        'answers': ['picture', 'imitation', 'split', 'blend'],
-        'correctAnswer': 'blend'
-      },
-      {
-        'questionText': "Which is a synonym of epidemic?",
-        'answers': ['artificial', 'swift', 'infectious', 'mocking'],
-        'correctAnswer': 'infectious'
-      },
-      {
-        'questionText': 'Which is a synonym of refrain?',
-        'answers': ['continue', 'abstain', 'subtract', 'permit'],
-        'correctAnswer': 'abstain'
-      },
-      {
-        'questionText': 'Which is a synonym of conjecture?',
-        'answers': ['approval', 'demonstration', 'weight', 'guess'],
-        'correctAnswer': 'guess'
-      }
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My App'),
         ),
-        body: Column(
-          children: [
-            Question(
-              questions[_questionIndex]['questionText'],
-            ),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((option) {
-              return Answer(option, _answerQuestion);
-            }).toList()
-          ],
-        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  Question(
+                    questions[_questionIndex]['questionText'],
+                  ),
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      .map((option) {
+                    return Answer(option, _answerQuestion);
+                  }).toList()
+                ],
+              )
+            : Center(
+                child: Text("You are done"),
+              ),
       ),
     );
   }
